ca7b81c875c06b300796b3af6d7053fd56627bdd
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-07-18 21:18:39 +0100

    feat(0x01): add function `insert_school`
    It inserts a new document into the collection.

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains the function `insert_school`
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
